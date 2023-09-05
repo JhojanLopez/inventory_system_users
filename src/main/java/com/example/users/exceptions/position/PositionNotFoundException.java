c22aaa180880c35a98ceec82032919d4c243f4f7
JhojanLopez <jhojanlopez327@gmail.com>
2023-09-05 09:09:17 -0500

    Revert "se agrega service position y pruebas unitarias"
    This reverts commit 64e9924aa046cfbbfc3d3e8a96793f6f07102824.

--- src/main/java/com/example/users/exceptions/position/PositionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.users.exceptions.position;
-
-public class PositionNotFoundException extends Exception{
-    public PositionNotFoundException(String message){
-        super(message);
-    }
-}
