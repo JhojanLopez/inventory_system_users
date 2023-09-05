71fab10d693ef85d27ec0dc5a1addffbdd061840
JhojanLopez <jhojanlopez327@gmail.com>
2023-09-04 21:53:08 -0500

    se mapea entidades

---
+++ src/main/java/com/example/users/database/entities/Position.java
@@ -0,0 +1,36 @@
+package com.example.users.database.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "positions")
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_position")
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/example/users/database/entities/User.java
@@ -0,0 +1,50 @@
+package com.example.users.database.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @JoinColumn(name = "id_position", referencedColumnName = "id_position", nullable = false)
+    @ManyToOne
+    private Position position;
+
+    @Column(nullable = false)
+    private LocalDate dateEntry;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @JoinColumn(name = "update_by", referencedColumnName = "id_user")
+    @ManyToOne
+    private User updateBy;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updatedAt;
+}
