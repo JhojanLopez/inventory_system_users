75cf849b1725adfe8285d4f4c77ad8d0c2099528
JhojanLopez <jhojanlopez327@gmail.com>
2023-09-05 11:33:33 -0500

    se modifica error al no tener constructores

--- src/main/java/com/example/users/models/UserDto.java
+++ src/main/java/com/example/users/models/UserDto.java
@@ -1,12 +1,16 @@
 package com.example.users.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private long id;
     private String name;
